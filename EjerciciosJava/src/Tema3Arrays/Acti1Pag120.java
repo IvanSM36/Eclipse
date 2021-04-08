8f99720d5cc1bafbb57988a3b8e8b4f0287f35d7
IvanSM36 <ivan_sm@hotmail.es>
2021-04-08 20:17:32 +0200

    array acti12

--- EjerciciosJava/src/Tema3Arrays/Acti1Pag120.java
+++ EjerciciosJava/src/Tema3Arrays/Acti1Pag120.java
@@ -4,11 +4,12 @@ public class Acti1Pag120 {
 
 	public static void main(String[] args) {
 
-		int[] array;
-		array = new int[12];
+		int[] array = new int[12];
 		
 		for(int i = 0; i < array.length; i++) {
-			System.out.println(i);
+			array[i] = (int)Math.round(Math.random()*10);
+			System.out.println(array[i]); //comentario
 		}
 	}
+	
 }
